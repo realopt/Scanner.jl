34107ea1f9738ec127882e4376df537725aaf918
Vitor Nesello <vitornesello@gmail.com>
2018-11-02 15:35:56 +0100

    adding function to close io

--- src/main.jl
+++ src/main.jl
@@ -7,8 +7,11 @@ mutable struct BoolWrap
 	val::Bool
 end
 
-
 function Scan(filepath::AbstractString)
 	s = Scan(open(filepath, "r"))
 	return s
+end
+
+function finish_scan(scan::Scan)
+    close(scan.is)
 end
